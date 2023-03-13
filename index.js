091c260f23548f7e4a3b1057f141d2ac519019d2
Apichai <thunder_blow@outlook.com>
2023-03-13 10:20:36 +0700

    FINAL Addpop-up text and  mechanics functionality

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ when you mouse over the image and text of that image pop-up */
 
 const rock = document.querySelector('.rock');
 const weaponDisplay = document.querySelector('.weapon-display');
+const playerWeapon = document.querySelector('.player-weapon');
+const playerTotalScore = document.querySelector('.player-score');
+const aiWeapon = document.querySelector('.ai-weapon');
+const aiTotalScore = document.querySelector('.ai-score');
+const scoreDiscrip = document.querySelector('.score-discrip');
+
 
 rock.addEventListener('mouseover', () => {
   weaponDisplay.textContent = 'Rock';
@@ -41,6 +47,18 @@ scissors.addEventListener('mouseout', () => {
 
 /* pop-up text end here */
 
+/* create Player and AI weapon */
+let playerScore = 0;
+let aiScore = 0;
+
+function calcomputerAI(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+let computerAI = 0;
+let player = 0;
+
+/* create Player and AI weapon end here*/
+
 /* game mechanic fucntion */
 
 function calculateScore(myChoice, ai) {
@@ -48,24 +66,26 @@ function calculateScore(myChoice, ai) {
   if ((myChoice === 1 && ai === 3)
     || (myChoice === 2 && ai === 1)
     || (myChoice === 3 && ai === 2)) {
-
     playerScore++;
-    alert('Player WIN'+`${playerScore}`);
+    playerTotalScore.textContent = `Player: ${playerScore}/5`;
+    scoreDiscrip.textContent = 'You WIN';
   } else if (myChoice === ai) {
-    alert('Tie')
+    scoreDiscrip.textContent = 'TIE';
   } else {
     aiScore++;
-    alert('AI WIN'+`${aiScore}`)
-
+    aiTotalScore.textContent = `Computer: ${aiScore}/5`
+    scoreDiscrip.textContent = 'Computer WIN';
   }
-    
-  if(playerScore === 5){
+
+
+
+  if (playerScore === 5) {
     alert('Player WIN')
 
     setTimeout(() => {
       location.reload();
     });
-  }else if (aiScore === 5){
+  } else if (aiScore === 5) {
     alert('AI WIN')
 
     setTimeout(() => {
@@ -73,59 +93,73 @@ function calculateScore(myChoice, ai) {
     });
   }
 
+
+
 }
 
 
 /* game mechanic fucntion end here*/
 
-/* create Plater and AI weapon */
-
-let player = 0;
-
-function computerAI(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-let playerScore = 0;
-let aiScore = 0;
-
-/* create Plater and AI weapon end here*/
 
 
-
-// /* add click event */
+/* add click event */
 function clickEvent() {
   rock.addEventListener('click', () => {
     player = 1;
-    calculateScore(player, computerAI(1,3))
+    computerAI = calcomputerAI(1, 3);
+    playerWeapon.src = 'img/weapon/Rock.png';
+    calculateScore(player, computerAI);
+    changAI_Img(computerAI);
+    playerWeapon.classList.add('activeimg');
+    aiWeapon.classList.add('activeimg');
   });
 
   paper.addEventListener('click', () => {
     player = 2
-    calculateScore(player, computerAI(1,3))
-
+    computerAI = calcomputerAI(1, 3);
+    playerWeapon.src = 'img/weapon/Paper.png';
+    calculateScore(player, computerAI);
+    changAI_Img(computerAI);
+    playerWeapon.classList.add('activeimg');
+    aiWeapon.classList.add('activeimg');
   });
 
   scissors.addEventListener('click', () => {
     player = 3;
-
-    calculateScore(player, computerAI(1,3))
-
+    computerAI = calcomputerAI(1, 3);
+    playerWeapon.src = 'img/weapon/Scissors.png';
+    calculateScore(player, computerAI);
+    changAI_Img(computerAI);
+    changAI_Img(computerAI);
+    playerWeapon.classList.add('activeimg');
+    aiWeapon.classList.add('activeimg');
   });
-
 
 
 }
 /* Player click event end here */
+/* chang ai img here*/
 
-/* changin content */
+function changAI_Img(computerAI_) {
+  if (computerAI_ === 1) {
+    aiWeapon.src = 'img/weapon/Rock.png';
+  } else if (computerAI_ === 2) {
+    aiWeapon.src = 'img/weapon/Paper.png';
+  } else if (computerAI_ === 3) {
+    aiWeapon.src = 'img/weapon/Scissors.png';
 
-/* changin content end here*/
+  }
+
+}
+
+/* chang ai img end here*/
 
 
 /* game run here*/
 
 clickEvent();
+
+
 
 /* game end here*/
 
@@ -142,40 +176,6 @@ clickEvent();
 
 
 
-/* Add computer-ai interact */
-
-
-// computerAI = weaponArray[randomNumber];
-
-
-// /* Add computer-ai interact end here*/
-
-
-// /* add click event */
-
-
-// const playerWeapon = document.querySelector('.player-weapon');
-
-// rock.addEventListener('click', () => {
-//   playerWeapon.src = 'img/weapon/Rock.png';
-//   playerWeapon.classList.add('activePlayerRock');
-//   player = weaponArray[0];
-// });
-
-// paper.addEventListener('click', () => {
-//   playerWeapon.src = 'img/weapon/Paper.png';
-//   playerWeapon.classList.add('activePlayerRock');
-//   player = weaponArray[1];
-// });
-
-// scissors.addEventListener('click', () => {
-//   playerWeapon.src = 'img/weapon/Scissors.png';
-//   playerWeapon.classList.add('activePlayerRock');
-//   player = weaponArray[2];
-// });
-
-
-/* Player click event end here */
 
 
 
